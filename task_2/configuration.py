998d909cb8bb660f7e41587df9baee1daad02eb1
Stacy <crazykazacatt@yandex.ru>
2025-05-21 19:54:03 +0300

    Add files via upload

---
+++ task_2/configuration.py
@@ -0,0 +1,4 @@
+# Королева Анастасия, 30-я когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://8e4abc29-02a1-477d-859a-90113af124c3.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
